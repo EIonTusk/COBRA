859dd24cd044cc147eb562f7dca9fc67252a595a
EIonTusk <eiontusk@github.com>
2026-04-23 14:39:22 +0200

    fix(autobuild): remove remaining max caps on scan/fanout/depth inputs

--- src/routes/autobuild/+page.svelte
+++ src/routes/autobuild/+page.svelte
@@ -604,7 +604,6 @@
 							id="cc-max"
 							type="number"
 							min={10}
-							max={500}
 							bind:value={chesscomMaxGames}
 							disabled={running}
 							class="font-mono"
@@ -870,7 +869,6 @@
 							id="fanout"
 							type="number"
 							min={1}
-							max={4}
 							bind:value={opponentFanout}
 							disabled={running}
 							class="font-mono"
@@ -886,7 +884,6 @@
 						id="depth"
 						type="number"
 						min={4}
-						max={40}
 						bind:value={depth}
 						disabled={running}
 						class="font-mono"
